cace71ee2c254339757de388b5e6a1db08dee4fe
Fabio <fabio.rmrezende@gmail.com>
2015-02-02 09:40:54 -0300

    primeiro commit
    Projetos em csharp

---
+++ Orcamento/BL/UsuariosBL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orcamento.DAL;
+using Orcamento.Modelos;
+
+namespace Orcamento.BL
+{
+    public class UsuariosBL
+    {
+
+        #region Verifca Usuario
+
+        public Usuario VerificaUsuario(string login, string senha)
+        {
+            UsuariosDAL usuariosDal = new UsuariosDAL();
+            return usuariosDal.VerificaUsuario(login, senha);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Orcamento/DAL/UsuariosDAL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Orcamento.Modelos;
+
+namespace Orcamento.DAL
+{
+    public class UsuariosDAL
+    {
+
+        #region Valida Usuario na tela de login
+
+        /// <summary>
+        /// Método para verificar se login e senha estão corretos
+        /// </summary>
+        /// <param name="login">Login do Usuário</param>
+        /// <param name="senha">Senha do Usuário</param>
+        /// <returns></returns>
+        public Usuario VerificaUsuario(string login, string senha)
+        {
+            ModelosDataContext us = new ModelosDataContext();
+
+            try
+            {
+                var user = (from usuarios in us.Usuarios
+                            where usuarios.usuarioNome == login
+                            && usuarios.usuarioSenha == senha
+                            && usuarios.usuarioAtivo == 1
+                            select usuarios).FirstOrDefault();
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+
+    }
+}
